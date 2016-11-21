8c8cd91553829f52acfd1a1ffdb4a00664bed9c9
esandine <esandine@stuy.edu>
2016-11-21 10:39:26 -0500

    Made modular function

--- exec.c
+++ exec.c
@@ -3,7 +3,10 @@
 #include <string.h>
 #include <unistd.h>
 
-int numspace(char* str){
+/**
+ * Counts the number of spaces in a string and returns it
+ */
+int num_spaces(char* str){
   int spaces = 0;
   while(*str){
     if(*str==' ')
@@ -12,13 +15,17 @@ int numspace(char* str){
   }
   return spaces;
 }
-int main(){
+
+/**
+ * Runs a command from stdin
+ */
+int run_cmd(){
   char input[42];
   fgets(input, sizeof(input), stdin);
   *strstr(input,"\n")=0;
-  printf("Input: %s\n",input);
-  printf("Spaces: %d\n", numspace(input));
-  char ** parse = (char **)calloc(sizeof(char *), numspace(input) + 2);
+  //printf("Input: %s\n",input);
+  //printf("Spaces: %d\n", num_spaces(input));
+  char ** parse = (char **)calloc(sizeof(char *), num_spaces(input) + 2);
   char ctr = 0;
   char* next = input;
   while(next){
@@ -27,10 +34,13 @@ int main(){
   }
   ctr = 0;
   while(parse[ctr]){
-    printf("%s\n", parse[ctr]);
+    //printf("%s\n", parse[ctr]);
     ctr++;
   }
   execvp(parse[0],parse);
   free(parse);
   return 0;
+}
+int main(){
+  run_cmd();
 }
